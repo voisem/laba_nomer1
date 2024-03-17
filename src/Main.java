8fec8efd9047c6b9a17b9197dddccba6622520e9
voisem <voisemyt@gmail.com>
2024-03-17 23:45:38 +0700

    сделал main из которго вызываются наши задания написал решение задания 'a'

--- src/Main.java
+++ src/Main.java
@@ -3,31 +3,37 @@ import java.util.Scanner;
 
 
 class Nalitov_a {
+    // ввод строки с клавиатуры, подсчет гласных букв и вывода результата
     static void str_in_and_out() {
+        System.out.print("Введите строку: ");
 
-        System.out.print("введите строку: ");
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+        Scanner scanner = new Scanner(System.in); // Создаем объект Scanner для считывания ввода с клавиатуры
+        String input = scanner.nextLine(); // Считываем введенную строку
 
         int out = count_glasnih(input);
-
-        System.out.print("Кол-во гласных в строке = " + out);
+        // Выводим результат подсчета гласных букв
+        System.out.print("Количество гласных в строке = " + out);
     }
 
+    // подсчет
     public static int count_glasnih(String input) {
-        int count = 0;
-        input = input.toLowerCase();
 
-        String glasnie = "aeiouy";
+        int count = 0; // переменная для хранения количества гласных букв
+        input = input.toLowerCase(); // строку в нижний регистр
 
+        String glasnie = "aeiouy"; // все гласные буквы
+
+        // Проходим по каждому символу
         for (int i = 0; i < input.length(); i++) {
             char x = input.charAt(i);
-
+            // Проверяем является ли текущий символ гласной буквой
             if (glasnie.contains(String.valueOf(x))) {
-                count++;
+
+                count++; // увеличиваем счетчик
             }
         }
-        return count;
+
+        return count; // Возвращаем количество гласных
     }
 }
 
